da7701e1f9608caa4c69643a0ece5b33103c0bbf
Chris Ellis <chris@clicksend.com>
2018-11-02 15:19:23 +0800

    removed redundant dirs

--- start_service.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-export GOLANG_VERSION=${GOLANG_VERSION:-1.11.1}
-export GOROOT=/libpostal/go
-export GOPATH=/libpostal/workspace
-export PATH=$PATH:/libpostal/go/bin
-
-/libpostal/bin/libpostal-rest
